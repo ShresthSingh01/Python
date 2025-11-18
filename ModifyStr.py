3de72c3497e7074395a4dc61d429ba5ea98fe491
SHRESTH SINGH <shresthsingh112233@gmail.com>
2025-11-18 20:56:45 +0530

    Add files via upload

---
+++ ModifyStr.py
@@ -0,0 +1,12 @@
+a="Hey guys I'm Shresth"
+#For upper case
+print(a.upper())
+#for lower case
+print(a.lower())
+#for replace
+print(a.replace("Hey","Hello"))
+#for strip
+b="   Hey guys I'm Shresth   "
+print(b.strip())
+#for split
+print(a.split())
