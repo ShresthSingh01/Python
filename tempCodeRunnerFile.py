cb8e6f3931244b1b4821d1e853b3e647a03a542d
SHRESTH SINGH <shresthsingh112233@gmail.com>
2025-12-21 21:44:34 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,14 @@
-y="Norway")
-print(thisdict1)
+for x in range(6):
+    print(x)
+
+x = range(10)
+print(x)
+for n in x:
+    print(n)
+    break
+
+
+x1 = range(3, 10)
+for n in x1:
+    print(n)
+    break
